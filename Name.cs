73826a3fc29bfcb4195b492b129884e09f84c1aa
dohuid <dohui2273@gmail.com>
2023-03-28 14:07:16 +0900

    int & bool 사칙연산

--- Name.cs
+++ Name.cs
@@ -17,7 +17,7 @@ namespace gamemathstudy
             bool isHightLevel = (level >= 40);
             // a = 살아있는 고렙 유저인가요?
             bool a = isAlive && isHightLevel;
-            // b = 살아있거나, 고랩 유저이거나, 둘 중 하나인가요?
+            // b = 살아있거나, 고랩 유저이거나 둘 중 하나인가요?
             bool b = isAlive || isHightLevel;
             // c = 죽은 유저인가요?
             bool c = !isAlive;
