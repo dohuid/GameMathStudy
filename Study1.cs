445080fd7034b990b1c38b129d9bab8e9c416648
dohuid <dohui2273@gmail.com>
2023-03-28 14:15:18 +0900

    enum타입 선언후 swirch~case문

---
+++ Study1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamemathstudy
+{
+    class Study1
+    {
+        enum CAL { SUM, SUB, MUL, DIV}
+        //열거형 0부터 시작
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            CAL cal = CAL.SUB;
+
+            switch (cal)
+            {
+                case CAL.SUM:
+                    Console.WriteLine(a + b);
+                    break;
+                case CAL.SUB:
+                    Console.WriteLine(a - b);
+                    break;
+                case CAL.MUL:
+                    Console.WriteLine(a * b);
+                    break;
+                case CAL.DIV:
+                    Console.WriteLine(a / b);
+                    break;
+            }
+        }
+    }
+}
