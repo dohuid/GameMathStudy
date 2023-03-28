030ce429e5b378ce1957e47414d299b6de782627
dohuid <dohui2273@gmail.com>
2023-03-28 14:06:32 +0900

    HelloWorld

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace gamemathstudy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Helloworld");
         }
     }
 }
