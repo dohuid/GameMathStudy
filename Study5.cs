3edadffc46da987c789d5c8a1df6b96660ab5f0d
dohuid <dohui2273@gmail.com>
2023-03-29 10:56:59 +0900

    리스트와 foreach

---
+++ Study5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gamemathstudy
+{
+    class Study5
+    {
+        static void Main(string[] args)
+        {
+            ArrayList number = new ArrayList();
+
+            for(int i = 0; i < 10; i++)
+            {
+                number.Add(i); //리스트에 0~9까지 넣기
+            }
+
+            foreach(int num in number)
+            {
+                Console.WriteLine(num); //리스트 출력
+            }
+        }
+    }
+}
