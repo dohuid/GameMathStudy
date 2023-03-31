e96323fa4367e16d33cc52831732951af17edcfa
dohuid <dohui2273@gmail.com>
2023-03-31 09:35:00 +0900

    윈도우 라이브러리 사용

---
+++ Study6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace gamemathstudy
+{
+    class Study6
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Welcome\nto\nc#\nProgramming!");
+        }
+    }
+}
